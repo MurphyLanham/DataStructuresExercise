493b6fd53769718183fbc80423f10af07a1307fa
Murphy Lanham <murphylanham@KCCCs-MacBook-Pro.local>
2021-03-03 11:12:23 -0500

    Corrections Made

--- dataStructuresExercise.js
+++ dataStructuresExercise.js
@@ -9,7 +9,15 @@ const sithLords = ["Darth Vader","Darth Sidious","Darth Maul"]
 const imperialOfficers = ["Grand Moff Tarkin", "Orson Krennic"]
 const starWarsVillains = sithLords.concat(imperialOfficers)
 console.log(starWarsVillains.slice(0,2));
-const droids =[]
+const droids = {
+    astromech: "R2-D2",
+    protocol: "C-3PO",
+    assassin: "IG-88"
+}
+console.log(droids["astromech"]);
+console.log(droids.protocol);
+droids.assassin = "IG-11"
+console.log(droids);
 
 console.log(jedi);
 console.log(starWarsVillains);
